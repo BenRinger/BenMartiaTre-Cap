3c8e1276e74cf00e53b35585e0656fe12857cf79
Martia <pureromancebymartia@gmail.com>
2022-06-11 20:05:21 -0400

    welcome

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import BrewPage from './components/BrewPage';
 import Home from './components/Home';
 import FoodPage from './components/FoodPage';
+import About from './components/About';
+import Contact from './components/Contact';
 // Page & Component Imports
 import Navbar from './components/Navbar'
 
@@ -22,11 +24,17 @@ export default class App extends Component {
     <div className="App">
       <Router>
         <Navbar />
+        <Switch>
         <Route exact path='/' component={Home}/>
         {/* <Route path='/about' component={About}/>
-        <Route path='/Contact' component={Contact}/> */}
+        <Route path='/contact' component={Contact}/> */}
         <Route path='/food' component={FoodPage} />
         <Route path='/brew' component={BrewPage} />
+        <Route path='/home' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+       </Switch>
+
 
       </Router>
     </div>
